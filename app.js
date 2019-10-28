900d1509d71598c9d0245926ba95a5e48a5f1eee
Reina <r.clarkemitchell@gmail.com>
2019-10-27 20:23:54 -0400

    made a initialize game function
    Also the new game function

--- app.js
+++ app.js
@@ -2,21 +2,10 @@
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 
 
-let scores , roundScore, activePlayer;
+var scores , roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+initializeGame();
 
-
-document.querySelector('.dice').style.display = 'none';
-
-// Global Score
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-// Round Score
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 function nextPlayer(){
   document.querySelector('.dice').style.display = 'none';
@@ -53,7 +42,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 
   let currentPlayer = scores[activePlayer] += roundScore;
   document.getElementById(`score-${activePlayer}`).textContent = currentPlayer;
-  if(currentPlayer >= 100){
+  if(currentPlayer >= 20){
     document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
     document.querySelector('.dice').style.display = 'none';
     document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
@@ -61,10 +50,32 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
       nextPlayer()
   }
 
-
-  // if(currentPlayer === 100)
-  //if they hit hold, then it switches to other player
-  // score gets added to global score
-  //check if player won game
-
 });
+
+document.querySelector('.btn-new').addEventListener('click', initializeGame);
+
+
+function initializeGame(){
+  scores = [0,0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  document.querySelector('.dice').style.display = 'none';
+
+  // Global Score
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  // Round Score
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+
+document.querySelector(`.player-0-panel`).classList.remove('winner');
+document.querySelector(`.player-1-panel`).classList.remove('winner');
+document.querySelector(`.player-0-panel`).classList.remove('active');
+document.querySelector(`.player-1-panel`).classList.remove('active');
+document.querySelector(`.player-0-panel`).classList.add('active');
+
+}
